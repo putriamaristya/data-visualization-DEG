c64bc99e844b2bd9865691a0315a9212a3a98954
putri <amaristyaputri@gmail.com>
2024-03-28 00:51:21 +0700

    Initial commit

--- chorddiagram/chorddiagram.R
+++ chorddiagram/chorddiagram.R
@@ -1,88 +1,72 @@
----
-title: "chorddiagram"
-author: "putri"
-date: "2024-03-27"
-output: html_document
----
-
-# Import packages
-
-```{r}
 library("dplyr")
 library("readxl")
 library("corrplot")
 library("writexl")
 library("Hmisc")
 library("circlize")
-```
 
-## Load data and perform correlation analysis
-
-```{r}
+#get data
 dat <- read.delim("GSE116583.txt")
 
 dat <- dat %>%
   select(-Symbol)
 
+
 #pearson correlation
 result <- (rcorr(as.matrix(dat), type = c("pearson")))
 
-```
 
-## Prepare data for chord diagram
-
-```{r}
+#prepare data
 result_new <- result$r[-c(1:4), -c(5:12)]
 
 df = data.frame(from = rep(rownames(result_new), times = ncol(result_new)),
                 to = rep(colnames(result_new), each = nrow(result_new)),
                 value = as.vector(result_new),
                 stringsAsFactors = FALSE)
-```
 
-## Initialization for chord diagram
-```{r}
+
+#circular plot
 circos.clear()
 par(cex = 1) #label size
-```
 
-## 1 Chord diagram - simple and default
-```{r}
+
+# 1 Chord diagram - simple and default
+circos.clear()
 chordDiagram(df)
-```
 
-## 2 Chord diagram - corrected lables and title
-```{r}
-chordDiagram(df,
+
+# 2 Chord diagram - corrected lables and title
+circos.clear()
+chordDiagram(df, 
              annotationTrack = "grid",
              preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df))))))
 circos.track(track.index = 1, panel.fun = function(x, y) {
-  circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index,
+  circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
               facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
 }, bg.border = NA)
 
 title(main = "Naive vs. Transplant")
-```
 
-## 3 Chord diagram - visualize correlation strength based on color opacity
-```{r}
+
+# 3 Chord diagram - visualize correlation strength based on color opacity
+circos.clear()
 col_fun = colorRamp2(range(0:1), c("#FFEEEE", "#FF0000"), transparency = 0.5)
 
-chordDiagram(df,
-            col = col_fun,
-            annotationTrack = "grid",
-            preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df))))))
+chordDiagram(df, 
+             col = col_fun,
+             annotationTrack = "grid",
+             preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df))))))
 circos.track(track.index = 1, panel.fun = function(x, y) {
-  circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index,
+  circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
               facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
 }, bg.border = NA)
 
 title(main = "Naive vs. Transplant")
-```
 
 
-## 4 Chord diagram - positive and negative correlations
-```{r}
+# 4 Chord diagram - positive and negative correlations
+circos.clear()
+
 #changing one value only to show negative correlation (data does not contain negative correlation) -- ignore
 result_new[result_new == "0.968874116352948"] <- "-0.968874116352948"
 
@@ -90,9 +74,6 @@ df = data.frame(from = rep(rownames(result_new), times = ncol(result_new)),
                 to = rep(colnames(result_new), each = nrow(result_new)),
                 value = as.vector(result_new),
                 stringsAsFactors = FALSE)
-```
-
-```{r}
 
 #color
 col_mat <- ifelse(df$value > 0, 'red2',
@@ -109,21 +90,10 @@ circos.track(track.index = 1, panel.fun = function(x, y) {
               facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
 }, bg.border = NA)
 
-```
 
-
-## 5 Chord diagram - two chord diagrams (positive and negative correlations) + correlation strength
-```{r}
-#changing one value only to show negative correlation (data does not contain negative correlation) -- ignore
-result_new[result_new == "0.968874116352948"] <- "-0.968874116352948"
-
-df = data.frame(from = rep(rownames(result_new), times = ncol(result_new)),
-                to = rep(colnames(result_new), each = nrow(result_new)),
-                value = as.vector(result_new),
-                stringsAsFactors = FALSE)
-```
-
-```{r}
+# 5 Chord diagram - two chord diagrams (positive and negative correlations) + correlation strength
+circos.clear()
+set.seed(1)
 
 #color
 col_fun_pos = colorRamp2(range(0:1), c("#FFEEEE", "#FF0000"), transparency = 0.5)
@@ -132,37 +102,18 @@ col_fun_neg = colorRamp2(range(-1:0), c("#f0feff", "#0017d1"), transparency = 0.
 #chord diagram
 par(mfrow = c(1, 2))
 
-#positive correlation
-chordDiagram(df, 
-             col = col_fun_pos, 
-             grid.col = 1:12, 
-             link.visible = df$value > 0,
-             annotationTrack = "grid", 
-             preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df))))))
+#pos cor
+chordDiagram(df, col = col_fun_pos, grid.col = 1:12, link.visible = df$value > 0,annotationTrack = "grid", preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df))))))
 circos.track(track.index = 1, panel.fun = function(x, y) {
-  circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index,
+  circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
               facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
-}, bg.border = NA)
+}, bg.border = NA) # here set bg.border to NA is important
 title("Positive links")
 
-#negative correlation
-chordDiagram(df, 
-             col = col_fun_neg, 
-             grid.col = 1:12, 
-             link.visible = df$value < 0,
-             annotationTrack = "grid", 
-             preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df))))))
+#neg cor
+chordDiagram(df, col = col_fun_neg, grid.col = 1:12, link.visible = df$value < 0,annotationTrack = "grid", preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(df))))))
 circos.track(track.index = 1, panel.fun = function(x, y) {
-  circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index,
+  circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
               facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
-}, bg.border = NA)
+}, bg.border = NA) # here set bg.border to NA is important
 title("Negative links")
-```
-
-
-
-
-
-
-
-
